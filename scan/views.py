6e8fd0f4422a708e2e0636b049c651dd55fafe36
Hiromu Yakura <hiromu1996@gmail.com>
2013-07-05 20:32:25 +0900

    add new template for index

--- scan/views.py
+++ scan/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from system_scan.scan.models import *
+from django.http import HttpResponse
+from django.template import Context, loader
+
+def index(request):
+	contest_list = Contest.objects.all().order_by('-start')
+
+	template = loader.get_template('index.html')
+	context = Context({'contest_list': contest_list})
+	return HttpResponse(template.render(context))
+
