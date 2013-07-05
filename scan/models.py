c79e62a13a97bee1a3ef62784b20d9710818b26b
Hiromu Yakura <hiromu1996@gmail.com>
2013-07-05 20:12:34 +0900

    change the indent length

--- scan/models.py
+++ scan/models.py
@@ -2,61 +2,61 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	user_name = models.CharField(max_length = 50)
-	first_name = models.CharField(max_length = 50)
-	last_name = models.CharField(max_length = 50)
-	admin = models.BooleanField()
+    user_name = models.CharField(max_length = 50)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    admin = models.BooleanField()
 
-	def __unicode__(self):
-		return self.user_name
+    def __unicode__(self):
+        return self.user_name
 
 class Contest(models.Model):
-	name = models.CharField(max_length = 50)
-	start = models.DateTimeField()
-	end = models.DateTimeField()
+    name = models.CharField(max_length = 50)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 class Genre(models.Model):
-	name = models.CharField(max_length = 50)
+    name = models.CharField(max_length = 50)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 class Privilege(models.Model):
-	user = models.ForeignKey(User)
-	contest = models.ForeignKey(Contest)
+    user = models.ForeignKey(User)
+    contest = models.ForeignKey(Contest)
 
-	def __unicode__(self):
-		return self.user
+    def __unicode__(self):
+        return self.user
 
 class Problem(models.Model):
-	contest = models.ForeignKey(Contest)
-	genre = models.ForeignKey(Genre)
-	type = models.PositiveIntegerField()
-	statement = models.TextField()
-	option = models.TextField()
-	result = models.TextField()
-	point = models.PositiveIntegerField()
+    contest = models.ForeignKey(Contest)
+    genre = models.ForeignKey(Genre)
+    type = models.PositiveIntegerField()
+    statement = models.TextField()
+    option = models.TextField()
+    result = models.TextField()
+    point = models.PositiveIntegerField()
 
-	def __unicode__(self):
-		return self.statement
+    def __unicode__(self):
+        return self.statement
 
 class Answer(models.Model):
-	user = models.ForeignKey(User)
-	problem = models.ForeignKey(Problem)
-	answer = models.TextField()
-	point = models.PositiveIntegerField()
+    user = models.ForeignKey(User)
+    problem = models.ForeignKey(Problem)
+    answer = models.TextField()
+    point = models.PositiveIntegerField()
 
-	def __unicode__(self):
-		return self.answer
+    def __unicode__(self):
+        return self.answer
 
 class Score(models.Model):
-	user = models.ForeignKey(User)
-	contest = models.ForeignKey(Contest)
-	genre = models.ForeignKey(Genre)
-	score = models.PositiveIntegerField()
+    user = models.ForeignKey(User)
+    contest = models.ForeignKey(Contest)
+    genre = models.ForeignKey(Genre)
+    score = models.PositiveIntegerField()
 
-	def __unicode__(self):
-		return self.score
+    def __unicode__(self):
+        return self.score
