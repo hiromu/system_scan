04dae755ad16fb3df4965043169684d97aafe362
Hiromu Yakura <hiromu1996@gmail.com>
2013-07-09 02:49:29 +0900

    change to pass request to template

--- scan/views.py
+++ scan/views.py
@@ -4,13 +4,13 @@ from system_scan.scan.models import *
 from django.contrib.auth.views import login
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render_to_response
-from django.template import Context, loader
+from django.template import Context, RequestContext, loader
 
 def index(request):
 	contest_list = Contest.objects.all().order_by('-start')
 
 	context = {'contest_list': contest_list}
-	return render_to_response('index.html', context)
+	return render_to_response('index.html', context, RequestContext(request))
 
 def custom_login(request):
 	if request.user.is_authenticated():
