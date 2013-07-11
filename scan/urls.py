fd3926c91a051b63c567dfebe6251f8d142185d2
naoya <mine@mine-studio.com>
2013-07-11 16:32:29 +0900

    Change URL matching

--- scan/urls.py
+++ scan/urls.py
@@ -8,6 +8,9 @@ urlpatterns = patterns('',
     (r'^$', 'scan.views.index'),
 
     (r'^(?P<contest_id>\d+)/$', 'scan.views.contests.index'),
+    (r'^(?P<contest_id>\d+)/settings/$', 'scan.views.contests.settings_index'),
+    (r'^(?P<contest_id>\d+)/settings/(?P<tab>\w*)/$', 'scan.views.contests.settings'),
+    (r'^(?P<contest_id>\d+)/settings/user_del/(?P<user_id>\d+)/$', 'scan.views.contests.user_del_settings'),
 #   (r'^(?P<contest_id>\d+)/(?P<genre_id>\d+)/$', 'scan.views.answer'),
 #   (r'^(?P<contest_id>\d+)/score/$', 'scan.views.score'),
 
@@ -20,12 +23,7 @@ urlpatterns = patterns('',
 #   (r'^mark/(?P<contest_id>\d+)/(?P<user_id>\d+)/(?P<genre_id>\d+)/$', 'scan.views.mark'),
 
     (r'^settings/$', 'scan.views.settings.index'),
-    (r'^settings/(?P<contest_id>\d+)/$', 'scan.views.settings.contest'),
     (r'^settings/(?P<contest_id>\d+)/(?P<genre_id>\d+)/$', 'scan.views.settings.problem'),
-    (r'^settings/(?P<contest_id>\d+)/genre/$', 'scan.views.settings.genre'),
-    (r'^settings/(?P<contest_id>\d+)/user/$', 'scan.views.settings.user'),
-    (r'^settings/(?P<contest_id>\d+)/user/add/$', 'scan.views.settings.user_add'),
-    (r'^settings/(?P<contest_id>\d+)/user/del/(?P<user_id>\d+)/$', 'scan.views.settings.user_del'),
 
 #   (r'^admin/$', 'scan.views.admin'),
     (r'^admin/database/', include(admin.site.urls)),
