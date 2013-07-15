3ae7a08191cc3b4bccb695e86d8ac34c6a58b639
Hiromu Yakura <hiromu1996@gmail.com>
2013-07-15 11:10:18 +0900

    change post login url

--- scan/views/accounts.py
+++ scan/views/accounts.py
@@ -2,6 +2,7 @@
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login as django_login
+from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 
@@ -9,8 +10,9 @@ def login(request):
     if request.user.is_authenticated():
         return redirect('scan.views.index')
     else:
-        return django_login(request, 'accounts/login.html')
+        context = {'next': reverse('scan.views.index')}
+        return django_login(request, 'accounts/login.html', extra_context = context)
 
 @login_required
 def profile(request):
-    return render_to_response('accounts/profile.html', context_instance=RequestContext(request))
+    return render_to_response('accounts/profile.html', context_instance = RequestContext(request))
