92da8997146463b58c57c90ce465c6f5883233e2
naoya <mine@mine-studio.com>
2013-07-20 18:37:12 +0900

    Use the slider only when there is more than one banner

--- scan/static/index.js
+++ scan/static/index.js
@@ -1,10 +1,12 @@
 $(function(){
-    $('#top-slider').easySlider({
-        auto: true,
-        pause: 6000,
-        continuous: true,
-        numeric: true,
-    });
-    $('#controls').addClass('btn-group');
-    $('#controls').children().each(function(){$(this).addClass('btn')});
+    if($('#top-slider>ul>li').size() > 1) {
+        $('#top-slider').easySlider({
+            auto: true,
+            pause: 6000,
+            continuous: true,
+            numeric: true,
+        });
+        $('#controls').addClass('btn-group');
+        $('#controls').children().each(function(){$(this).addClass('btn')});
+    }
 });
