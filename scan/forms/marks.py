99141d3e6b217124f30ad941c9a028da43834393
Hiromu Yakura <hiromu1996@gmail.com>
2013-07-29 23:22:42 +0900

    improve mark page

--- scan/forms/marks.py
+++ scan/forms/marks.py
@@ -5,7 +5,7 @@ import json
 from scan.models import Answer
 
 from django.forms import CharField, CheckboxSelectMultiple, ModelForm, ChoiceField, ModelChoiceField, RadioSelect, TextInput, Textarea, IntegerField
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 class AnswerForm(ModelForm):
     class Meta:
@@ -29,8 +29,10 @@ class AnswerForm(ModelForm):
 
         self.fields['answer'].label = problem.title
         self.fields['answer'].help_text = problem.statement
-        self.fields['answer'].widget.attrs['readonly'] = True
+        self.fields['answer'].widget.attrs['disabled'] = True
         self.fields['answer'].required = False
+
+        self.fields['point'].help_text = '(%s: %d)' % (ugettext(u'満点'), problem.point)
 
     def clean(self, *args, **kwargs):
         cleaned_data = super(AnswerForm, self).clean(*args, **kwargs)
@@ -40,7 +42,7 @@ class AnswerForm(ModelForm):
 
         point = cleaned_data.get('point')
         if point > self.problem.point:
-                self._errors['point'] = self.error_class([_(u'満点は%d点です。' % self.problem.point)])
+                self._errors['point'] = self.error_class([_(u'満点は%d点です。') % self.problem.point])
                 del cleaned_data['point']
 
         return cleaned_data
