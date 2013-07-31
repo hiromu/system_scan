762f32e3b5e4736f7b39c1f2c54751fb27d703d2
Hiromu Yakura <hiromu1996@gmail.com>
2013-08-01 03:51:22 +0900

    change to hide percentage by javascript

--- scan/static/detail.js
+++ scan/static/detail.js
@@ -4,5 +4,22 @@ $(function(){
             top: (840.0 - parseFloat($(this).find('td:nth-of-type(4)').text()) * 800.0 / parseFloat($('#summary-max-score').val()) - 20.0) + 'px'
         }, 1000);
     });
-});
 
+    $('.category-summary').each(function(){
+        $(this).find('table').hide();
+        $(this).find('i.category').removeClass('icon-chevron-up');
+        $(this).find('i.category').addClass('icon-chevron-down');
+    });
+
+    $('div.category-summary').click(function(){
+        if ($(this).find('table').is(':hidden')) {
+            $(this).find('table').show();
+            $(this).find('i.category').removeClass('icon-chevron-down');
+            $(this).find('i.category').addClass('icon-chevron-up');
+        } else {
+            $(this).find('table').hide();
+            $(this).find('i.category').removeClass('icon-chevron-up');
+            $(this).find('i.category').addClass('icon-chevron-down');
+        }
+    });
+});
