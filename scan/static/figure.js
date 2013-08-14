4b98a409b45493568afd41c3e19bbfee858f97a2
naoya <mine@mine-studio.com>
2013-08-15 00:41:17 +0900

    New feature: Figure pop up

---
+++ scan/static/figure.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('body').append($('<div>').attr({id:'figure-dialog'}));
+});
+
+function showFigureDialog(t){
+    $('#figure-dialog').empty();
+    $('#figure-dialog').append($('<img>').attr({src:$(t).find('img').attr('src')}));
+    $('#figure-dialog').dialog({
+        title: $(t).find('figcaption').text(),
+        closeOnEscape: true,
+        modal: true,
+        width: 'auto',
+        height: 'auto',
+    });
+}
